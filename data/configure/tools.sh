5f871c80793dbd45908754e87479d46e0c6d46ef
Schmidely Stéphane <s.schmidely@gmail.com>
2020-04-04 18:05:42 +0200

    feaat(configuration): add system script

--- data/configure/tools.sh
+++ data/configure/tools.sh
@@ -41,3 +41,8 @@ pacman -Sy --noconfirm --needed \
 # tasksh \
 # timewarrior \
 # tmux-mem-cpu-load \
+
+echo 'Install Oh My ZSH' ; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo 'Install Vundle' ; git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# echo 'Install Vim plugins' ; vim +PluginInstall +qall
